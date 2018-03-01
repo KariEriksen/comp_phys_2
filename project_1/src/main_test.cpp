3f8efae1422c592fd41876b301695462396f036b
copperwire <solli.robert@gmail.com>
2018-03-01 18:00:40 +0100

    working classes

--- project_1/src/main_test.cpp
+++ project_1/src/main_test.cpp
@@ -1,10 +1,18 @@
-#include "vmc.h"
 #include <iostream>
+#include <armadillo>
+#include "vmc.h"
 
 using namespace std;
 
 int main(){
-    vmc obj(1, 1, 1e2, 0.1);
-    cout << "cunt" << endl;
+    /* Example usage 
+    *vmc vmc_solver(1, 1, 1e2, 0.1);   
+    *vmc_solver::solve("importance", "analytic", "none", false);
+    *vmc_solver::output();
+    */
+
+    vmc solver(1, 1, 1e2, 0.1);
+    solver.print_params();
+    solver.solve(-1, 2, 0.001, 0, 1, 0.1);
     return 0;
 }
--- project_1/src/vmc.h
+++ project_1/src/vmc.h
@@ -1,15 +1,44 @@
+using namespace arma;
+using namespace std;
+
 class vmc {
-    int n_dim, N, n_carlos;
-    double step;
+    public:
+        int n_dim, N, n_carlos;
+        double step;
+    
+    private: 
+        bool importance_sampling = false; 
+        bool gradient_desc = false; 
+        bool anal_derivative = false; 
+        bool interactive = false;
+        mat R;
+        mat R_p;
+        vector<vector<double>> simul_values;
+	double best_alpha;
+	double best_beta;
+	double best_mean_E;
+	double index_best;
 
     public :
         vmc(int n_dim, int N, int n_carlos, double step);
-        void solve(string sampling, string derivative, bool variatonal_search, bool interact);
+        void set_simul_params(bool imp, bool grad, bool anal, bool inter);
+        void print_params();
+        int solve(double alpha_start, double alpha_stop, double alpha_increment,
+        double beta_start, double beta_stop, double beta_increment);
         void output_result();
 
     private :
-        carlo_cycle(mat R, mat R_p, double alpha, double beta, bool importance);
-        variational_mc(double beta_start, double beta_stop, double beta_increment,
-                double alpha_start, double alpha_stop, double alpha_increment, bool gradient_desc);
+        vector<double> carlo(double alpha, double beta);
+        int variational_mc_naive(double beta_start, double beta_stop, double beta_increment,
+                double alpha_start, double alpha_stop, double alpha_increment 
+                );
+
+        void prop_move();
+        double local_energy_noninter(double alpha, double beta);
+        double prob_dens_ratio_noninter(mat R_curr, double alpha, double beta);
+        double prob_dens_ratio_inter(mat R_curr, double alpha, double beta);
+        vector<double> output(mat E_l);
+ 
+
 
 };
