8069245fb76fc2ec80aec7025ae8b338dc0688de
copperwire <solli.robert@gmail.com>
2018-03-19 15:14:22 +0100

    saturday work on IN-case

--- project_1/src/main_GIN.cpp
+++ project_1/src/main_GIN.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]){
     double alpha, beta, step, h, a; 
     int N_p, N_d, N_mc;
     alpha = 0.5; beta = 1; step = 0.1; h = 1e-5; a = 0.0043; 
-    N_p = 40; N_d = 1; N_mc = 1e6;
+    N_p = 10; N_d = 2; N_mc = 1e5;
     
     vector<double> params = {alpha, beta, h, a};
     g.set_params(params, N_d, N_p);
--- project_1/src/vmc.cpp
+++ project_1/src/vmc.cpp
@@ -13,7 +13,7 @@ using namespace std;
 vector<double> vmc::monte_carlo(WaveFunc *psi_t){
     vector<double> E_l; 
     E_l.push_back(psi_t -> E_l(R));
-
+    cout << "Initial energy: " << E_l[0] << endl;
     for(int i = 1; i < N_mc; i++){
         double tmp = metropolis_hastings(psi_t, E_l[i-1]);
         E_l.push_back(tmp);
@@ -35,18 +35,33 @@ void vmc::set_params(double a_in, double b_in, int N, int dim,int mc_cycles){
     gen = new mt19937(rd());
 }
 
-vector<double> vmc::solve(WaveFunc *psi_t){
-    // -> is dereferencing and  member access to methods of class.
-    cout << "pre ass" << endl;
-    uniform_real_distribution<double> dis (-1, 1);
+void vmc::generate_positions(double step_int){
+    uniform_real_distribution<double> dis (-N_p, N_p);
 
     for(int i = 0; i < N_p; i++){
         for(int j = 0; j < N_d; j++){
             //gen must be dereferenced to use
-            double tmp = dis(*gen) * step;
+            double tmp = dis(*gen) * step_int;
             R(i, j) = tmp;
         }
     }
+ 
+}
+
+vector<double> vmc::solve(WaveFunc *psi_t){
+    // -> is dereferencing and  member access to methods of class.
+    
+    generate_positions(step);
+    double evaluated = psi_t -> evaluate(R);
+    double step_init = 0;
+
+    while(evaluated == 0){
+        step_init += step;
+        generate_positions(step_init);
+        evaluated = psi_t -> evaluate(R);
+        cout << step_init << endl;
+    }
+    
     return monte_carlo(psi_t);
 }
 
