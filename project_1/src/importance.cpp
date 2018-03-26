a031824babdcf1e36444674216dde41f4e304f6a
Geir Ulvik <ulvik@usit.uio.no>
2018-03-26 16:35:50 +0200

    Changed assumed return type from drift_force to mat(1, N_d) and adjusted code to reflect this.

--- project_1/src/importance.cpp
+++ project_1/src/importance.cpp
@@ -15,15 +15,14 @@ double Importance::metropolis_hastings(WaveFunc *psi_t, double prev_E_l){
 	normal_distribution<> dis_zeta(0, 1);
 	
 
-    mat F_drift(1, N_d);
 
     int j = dis_r(*gen);
 	double zeta = dis_zeta(*gen);
+    mat F_drift(1, N_d);
+	F_drift = psi_t -> drift_force(R.row(j));
    
-	for(int i = 0; i < N_d; i++){
-		F_drift(i) = psi_t -> drift_force(R(j,i))
-        R_p(j, i) += 0.5*F_x*dt + zeta*sqrt(dt);
-    }
+	F_drift = psi_t -> drift_force(R(j,i))
+	R_p(j) += 0.5*F_drift*dt + zeta*sqrt(dt);
 
     double eps = dis_p(*gen);
     double P = psi_t -> ratio(R, R_p, j);
@@ -35,26 +34,25 @@ double Importance::metropolis_hastings(WaveFunc *psi_t, double prev_E_l){
     //is independent of psi, so I think not
 
 	mat F_drift_proposed(1, N_d);
-	for(int i = 0; i < N_d; i++)
-		F_drift_proposed(i) = psi_t -> drift_force(R_p(j,i));
+	F_drift_proposed(i) = psi_t -> drift_force(R_p.row(j));
 
     double term1 = 0;
     double term2 = 0;
     double term3 = 0;
 
-    double Green_x;
-    double Green_y;
+    mat Green_prev(1, N_d);
+    mat Green_proposed(1, N_d);
 
-    for(int i = 0; i < N_p; i++){
-		term1 = R_p(j, i) - R(j, i) - 0.5*dt*F_drift(i);
-		term2 = R(j, i) - R_p(j, i) - 0.5*dt*F_drift_proposed(i);
+    for(int j = 0; j < N_p; j++){
+		term1 = R_p.row(j) - R.row(j, i) - 0.5*dt*F_drift;
+		term2 = R.row(j) - R_p.row(j) - 0.5*dt*F_drift_proposed;
 		term3 = 2*dt;
 
-		Green_x += exp((-(term1*term1))/term3);
-		Green_y += exp((-(term2*term2))/term3);
+		Green_prev += exp((-(term1*term1))/term3);
+		Green_proposed += exp((-(term2*term2))/term3);
     }
 
-    double q = (Green_x/Green_y)*P;
+    mat q(1, N_d) = (Green_prev/Green_proposed)*P;
 
     if(eps < q){
         R = R_p;
