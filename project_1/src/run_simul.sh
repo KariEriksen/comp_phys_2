deb8c389835b614651655c4553511a609e46bfe3
copperwire <solli.robert@gmail.com>
2018-03-28 13:54:49 +0200

    just renaming stuff

--- project_1/src/run_simul.sh
+++ project_1/src/run_simul.sh
@@ -1,7 +1,7 @@
-N_mc="4";
+N_mc="15";
 for N_d in "1" "2" "3"
 do
-for N_p in "1" "10" "100" "500"
+for N_p in "1" "10" "100" "500" 
 do
     echo "simulating with Nd: $N_d and N_p: $N_p and Nmc: $N_mc" 
     ./app.x $N_p $N_d $N_mc 
