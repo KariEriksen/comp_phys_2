aa3dcdb83278f8a0b3a4241ac169030688160fa9
Geir Ulvik <ulvik@usit.uio.no>
2018-04-07 20:43:40 +0200

    Better, but still not there!

--- project_1/src/importance.cpp
+++ project_1/src/importance.cpp
@@ -13,23 +13,21 @@ double Importance::metropolis_hastings(WaveFunc *psi_t, double prev_E_l){
     uniform_real_distribution<double> dis_p(0, 1);
 	normal_distribution<double> dis_zeta(0.0, 1.0);
 
-
+	// Pick particle j to move.
     int j = dis_r(*gen);
-	
-	double zeta = dis_zeta(*gen);
+
+	// Calculate drift force for that particle.
     mat F_drift(1, N_d);
 	F_drift = psi_t -> drift_force(R.row(j));
-   
+
+	// Move the particle
+	double zeta = dis_zeta(*gen);
 	R_p.row(j) += 0.5*F_drift*dt + zeta*sqrt(dt);
 
     double P = psi_t -> ratio(R, R_p, j);
     P *= P;
 
-    //R = R_p;
-    //psi_t -> update();
-    //Do we need to update the wave equation? The drift force
-    //is independent of psi, so I think not
-
+	// Calculate drift force for particle j in proposed position
 	mat F_drift_proposed(1, N_d);
 	F_drift_proposed = psi_t -> drift_force(R_p.row(j));
 
@@ -40,14 +38,13 @@ double Importance::metropolis_hastings(WaveFunc *psi_t, double prev_E_l){
     double Green_prev;
     double Green_proposed;
 
-    for(int i = 0; i < N_p; i++){
-		term1 = R_p.row(i) - R.row(i) - 0.5*dt*F_drift;
-		term2 = R.row(i) - R_p.row(i) - 0.5*dt*F_drift_proposed;
-		term3 = 2*dt;
+	// Calculate greens functions
+	term1 = R_p.row(j) - R.row(j) - 0.5*dt*F_drift;
+	term2 = R.row(j) - R_p.row(j) - 0.5*dt*F_drift_proposed;
+	term3 = 2*dt;
 
-		Green_prev += exp((-(dot(term1,term1)))/term3);
-		Green_proposed += exp((-(dot(term2,term2)))/term3);
-    }
+	Green_prev += exp((-(dot(term1,term1)))/term3);
+	Green_proposed += exp((-(dot(term2,term2)))/term3);
 
 	double q = (accu(Green_prev)/accu(Green_proposed))*P;
     double eps = dis_p(*gen);
