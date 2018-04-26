7d6c46b63fc0fe93802e5fd8b1fe56795ffbb8bf
copperwire <solli.robert@gmail.com>
2018-04-26 18:09:58 +0200

    added option for one-body estimation

--- project_1/src/vmc.cpp
+++ project_1/src/vmc.cpp
@@ -4,6 +4,8 @@
     #include <armadillo>
 #endif
 
+#include <math.h>
+#include <stdlib.h>  
 #include <ctime>
 #include <random>
 #include "../include/vmc.h"
@@ -25,19 +27,28 @@ void vmc::monte_carlo(WaveFunc *psi_t, metadata *exp_vals){
        if(compute_obd){
             /*r = sqrt(x^2 + y^2)*/
             mat all_radii= sqrt(sum(square_R, 1));
-            count_obd(all_radii, *exp_vals);
+            count_obd(all_radii, exp_vals);
        }
 
     for(int i = 1; i < N_mc; i++){
         double tmp = metropolis_hastings(psi_t, exp_vals -> exp_E[i-1]);
-        exp_vals -> exp_E[i] = tmp;
 
         if(compute_extra || compute_obd){
+            square_R = (tmp == exp_vals -> exp_E[i-1]) ? square_R : square(R);
 
-            double prod_r_cur = (double) as_scalar(accu(square(R)));
-            exp_vals -> prod_R[i] = prod_r_cur;
-            exp_vals -> prod_R_exp_E[i] = prod_r_cur * tmp;
+            if(compute_extra){
+                double prod_r_cur = (double) as_scalar(accu(square_R)); 
+                exp_vals -> prod_R[i] = prod_r_cur;
+                exp_vals -> prod_R_exp_E[i] = prod_r_cur * exp_vals -> exp_E[i];
+            }
+            if(compute_obd){
+                /*r = sqrt(x^2 + y^2)*/
+                mat all_radii= sqrt(sum(square_R, 1));
+                count_obd(all_radii, exp_vals);
         }
+       
+        exp_vals -> exp_E[i] = tmp;
+    }
     }
 }
 
@@ -71,22 +82,32 @@ void vmc::generate_positions(double step_int){
 }
 
 void vmc::count_obd(mat radii, metadata* all_exp){
-    
+    int sumval = 0;
     for(int i = 0; i < N_p ; i++){
         double tmp_rad = radii[i];
-        for(int j = 0; j < obd_n_bins - 1; j++){
-            if(tmp_rad > obd_bins[j] && tmp_rad < obd_bins[j+1])
+        for(int j = 0; j < obd_n_bins ; j++){
+            double tmp_bin = obd_bins[j];
+            if((tmp_rad > (tmp_bin - bin_length)) && (tmp_rad < (tmp_bin + bin_length))){
+                sumval +=1;
                 all_exp -> obd[j] += 1;
+
                 break;
+            }
         }
     }
+
+    if(sumval != N_p){
+        cout << sumval << " | " << N_p << " | " <<  endl;
+        exit(EXIT_FAILURE);
+    }
+
 }
 
 
 vector<double> vmc::solve(WaveFunc *psi_t, string filename){
     // -> is dereferencing and  member access to methods of class.
     
-    obd_n_bins = 100;
+    obd_n_bins = 600;
 
     generate_positions(step);
     psi_t -> initialize(R);
@@ -116,13 +137,19 @@ vector<double> vmc::solve(WaveFunc *psi_t, string filename){
     }
 
     if(compute_obd){
-        all_exp.obd = new int[obd_n_bins];
+        all_exp.obd = new long int[obd_n_bins];
         obd_bins = new double[obd_n_bins];
 
-        double outer_limit = 5;
+        for(int i = 0; i<obd_n_bins; i++){
+            all_exp.obd[i] = 0;
+            obd_bins[i] = 0;
+        }
+
+        double outer_limit = 6;
+        bin_length = outer_limit / (double)obd_n_bins;
         
         for(int i = 0; i<obd_n_bins; i++){
-            obd_bins[i] = i * outer_limit / obd_n_bins;
+            obd_bins[i] = (i+1) * bin_length;
         }
     }
 
@@ -144,16 +171,61 @@ vector<double> vmc::solve(WaveFunc *psi_t, string filename){
     } 
 
     
-    string header = "# N_p: " + to_string(N_p) 
-        + "| N_d: " + to_string(N_d) 
-        + "| N_mc: " + to_string(N_mc) ;
+    string header = "#{N_p: " + to_string(N_p) 
+        + ", N_d: " + to_string(N_d) 
+        + ", N_mc: " + to_string(N_mc)
+        + ", alpha: " +to_string(a)
+        + ", beta: "+ to_string(b) + "}";
 
-    ofstream output_file(filename);
+    ofstream output_file("../data/"+filename);
     
     output_file << header << endl;
     arma_e_l.save(output_file, csv_ascii); 
-
+    
+    cout << "Writing local energies to file: " << "../data/"+  filename << endl;
     output_file.close();
+
+    if(compute_obd){
+        string obd_filename = "../data/obd_"+filename;
+        ofstream obd_output(obd_filename);
+        double* divisor = new double[obd_n_bins];
+
+        if(N_d == 1){
+            divisor[0] = bin_length * N_mc;
+        }
+        else if(N_d == 2){
+            divisor[0] = M_PI * N_mc* (obd_bins[0] * obd_bins[0] ); 
+        }
+        else{
+            divisor[0] = (4.0/3)*M_PI * N_mc* (pow(obd_bins[0], 3) );
+        }
+
+        for(int i = 1; i < obd_n_bins ; i++){
+            if(N_d == 1){
+                divisor[i] = bin_length * N_mc;
+            }
+            else if(N_d == 2){
+                divisor[i] = M_PI * N_mc* (obd_bins[i] * obd_bins[i] 
+                        - obd_bins[i-1] * obd_bins[i-1] ); 
+            }
+            else{
+                divisor[i] = (4.0/3)*M_PI * N_mc* (pow(obd_bins[i], 3) 
+                        - pow(obd_bins[i-1],3) );
+            }
+        }
+        mat arma_obd = mat(obd_n_bins, 2);
+        
+        for(int i = 0; i< obd_n_bins; i++){
+            arma_obd(i, 1) = all_exp.obd[i]/divisor[i]; 
+            arma_obd(i, 0) = obd_bins[i];
+        }
+        
+        cout << "Writing obd to file: "<< obd_filename << endl;
+        obd_output << header << "\n";
+        arma_obd.save(obd_output, csv_ascii);
+        obd_output.close();
+    }
+
     
     vector<double> retval;
     if(compute_extra){
