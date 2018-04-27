1433e1d82b1ab848633935fd3d8f7ef25e3f3d92
copperwire <solli.robert@gmail.com>
2018-04-27 16:32:23 +0200

    nitpicks

--- project_1/post_anal/anal_g.py
+++ project_1/post_anal/anal_g.py
@@ -6,13 +6,21 @@ import seaborn as sns
 
 sns.set()
 
-obd_df =  np.loadtxt("../data/obd_test.csv", delimiter=",",  skiprows = 1)
+
+ni_filename = "../data/obd_NM_NIA_a_0.400000_b_1.000000_step_0.100000_np_100_nd_3.csv"  
+i_filename = "../data/obd_NM_IA_a_0.400000_b_1.000000_step_0.100000_np_100_nd_3.csv" 
+
+ni_obd_df =  np.loadtxt(ni_filename, delimiter=",",  skiprows = 1)
+i_obd_df =  np.loadtxt(i_filename, delimiter=",",  skiprows = 1)
 
 
+plt.hist(ni_obd_df[:,1], bins = ni_obd_df[:,0], label = "non interactive", alpha = 0.6)
+plt.hist(i_obd_df[:,1], bins = i_obd_df[:,0], label = "interactive", alpha = 0.6)
 
-plt.hist(obd_df[:,1], bins = obd_df[:,0])
 plt.xlabel(r" $r$")
 plt.ylabel(r"$\rho(r)$")  
+
+plt.legend()
 plt.show()
 
 
