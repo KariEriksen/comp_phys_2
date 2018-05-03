b552336d85d06ec578cebc7c47d175aecbe244af
Geir Ulvik <ulvik@usit.uio.no>
2018-05-03 23:30:39 +0200

    Added some dt support. Need to modify meta-file.

--- project_1/post_anal/anal_c_inter.py
+++ project_1/post_anal/anal_c_inter.py
@@ -60,123 +60,124 @@ mask = [not f.endswith("data.csv") for f in filenames]
 filenames = sorted(filenames[mask], key = lambda x: float(alpha_regex_obj.search(x).group(1)))
 stds = [0]
 
+dts = (0.001, 0.01, 0.1, 1.0)
 N_ps = (10,)
 N_mc = 0
 N_ds = (2, 3)
 sim_types = [v for v in sys.argv[1:]]
 
 fast = False 
-
-for N_d_i in N_ds:
-    for N_p_i in N_ps:
-        mean_n_times = pd.read_csv("../data/"+
-                #"_"+sim_types[1][3:]+
-                sim_types[0]+
-                "_meta"+
-                "_np_"+str(N_p_i)+
-                "_nd_"+str(N_d_i)+
-                "_data.csv")
-        
-        sim_type = sim_types[0]
-        tmp = []
-        
-        for i in range(len(sim_types)):
-            for filename in filenames: 
-                if filename.startswith(sim_type):
-                    t_filename = "../data/"+filename
-                    
-                    f_o = open(t_filename)
-                    header = f_o.readline()
-                    f_o.close()
-
-                    match_o = header_regex_obj.search(header)
-                    N_p = match_o.group(1)
-                    N_d = match_o.group(2)
-                    N_mc = match_o.group(3)
-
-                    if fast:
-                        break 
-                    elif int(N_p) == N_p_i and int(N_d) == N_d_i:
-                        A = loadtxt(t_filename, delimiter = "\n", skiprows = 1)
-                        tmp.append(block(A))
-            stds[i] = tmp
+for dt in dts:
+    for N_d_i in N_ds:
+        for N_p_i in N_ps:
+            mean_n_times = pd.read_csv("../data/"+
+                    #"_"+sim_types[1][3:]+
+                    sim_types[0]+
+                    "_meta"+
+                    "_np_"+str(N_p_i)+
+                    "_nd_"+str(N_d_i)+
+                    "_data.csv")
             
+            sim_type = sim_types[0]
+            tmp = []
+            
+            for i in range(len(sim_types)):
+                for filename in filenames: 
+                    if filename.startswith(sim_type):
+                        t_filename = "../data/"+filename
+                        
+                        f_o = open(t_filename)
+                        header = f_o.readline()
+                        f_o.close()
 
-        fig, ax = plt.subplots(figsize=(9, 7))
+                        match_o = header_regex_obj.search(header)
+                        N_p = match_o.group(1)
+                        N_d = match_o.group(2)
+                        N_mc = match_o.group(3)
 
-        time_anal = np.average(mean_n_times["analytic_time"])
-        '''
-        try:
-            time_num = np.average(mean_n_times["numeric_time"])
-            num_min_index = np.where(
-                    mean_n_times.numeric_energy == np.min(mean_n_times.numeric_energy))
-            num_min_cord = (mean_n_times.alpha[num_min_index[0][0]],
-                mean_n_times.numeric_energy[num_min_index[0][0]])
+                        if fast:
+                            break 
+                        elif int(N_p) == N_p_i and int(N_d) == N_d_i:
+                            A = loadtxt(t_filename, delimiter = "\n", skiprows = 1)
+                            tmp.append(block(A))
+                stds[i] = tmp
+                
 
-            t_r = time_num / time_anal
-        except IndexError:
-            pass 
-        '''
+            fig, ax = plt.subplots(figsize=(9, 7))
 
-        anal_min_index = np.where(
-                mean_n_times.analytic_energy == np.min(mean_n_times.analytic_energy))
-        anal_min_cord = (mean_n_times.alpha[anal_min_index[0][0]],
-                mean_n_times.analytic_energy[anal_min_index[0][0]])
-        
-        title =  sim_types[0]+ "_" + "VMC with "
-        title += r"$N_P = ${} | $N_D = ${} | $N_{{MC}}$ = {:.0e}".format(N_p_i, N_d_i, int(N_mc))
-        title += r"| time_analytic : ${T_A} = $" 
-        title += "{:.2e}".format(time_anal)
+            time_anal = np.average(mean_n_times["analytic_time"])
+            '''
+            try:
+                time_num = np.average(mean_n_times["numeric_time"])
+                num_min_index = np.where(
+                        mean_n_times.numeric_energy == np.min(mean_n_times.numeric_energy))
+                num_min_cord = (mean_n_times.alpha[num_min_index[0][0]],
+                    mean_n_times.numeric_energy[num_min_index[0][0]])
+
+                t_r = time_num / time_anal
+            except IndexError:
+                pass 
+            '''
+
+            anal_min_index = np.where(
+                    mean_n_times.analytic_energy == np.min(mean_n_times.analytic_energy))
+            anal_min_cord = (mean_n_times.alpha[anal_min_index[0][0]],
+                    mean_n_times.analytic_energy[anal_min_index[0][0]])
+            
+            title =  sim_types[0]+ "_" + "VMC with "
+            title += r"$N_P = ${} | $N_D = ${} | $N_{{MC}}$ = {:.0e}".format(N_p_i, N_d_i, int(N_mc))
+            title += r"| time_analytic : ${T_A} = $" 
+            title += "{:.2e}".format(time_anal)
 
 
-        if fast:
-            mean_n_times.plot("alpha", ["analytic_energy"],
-                    legend = True,
-                    title = title,
-                    ax = ax) 
-        else:
-            x = mean_n_times.alpha.values
-            #num = mean_n_times.numeric_energy.values
-            anal = mean_n_times.analytic_energy.values
+            if fast:
+                mean_n_times.plot("alpha", ["analytic_energy"],
+                        legend = True,
+                        title = title,
+                        ax = ax) 
+            else:
+                x = mean_n_times.alpha.values
+                #num = mean_n_times.numeric_energy.values
+                anal = mean_n_times.analytic_energy.values
+
+                '''
+                plt.errorbar(x, num, fmt = "x-",
+                        barsabove = True,
+                        yerr = np.array(stds[1], dtype = float),
+                        label = "numeric_energy")
+                '''
+                errs = np.array(stds[0], dtype=float)
+                print(type(errs))
+                print(np.shape(errs))
+                print(anal," ",x)
+
+                plt.errorbar(x, anal, fmt = "^-",
+                        barsabove = True,
+                        yerr = errs,
+                        label = "analytic_energy: dt = " + str(dt)) 
+                plt.title(title)
+                plt.xticks(x, rotation = 45, size = "medium")
+                plt.legend()
+
+            a_min_txt = r"Analytic GS @ $\alpha = ${} with $\langle E \rangle / N_P =$ {:.2f}".format(anal_min_cord[0], anal_min_cord[1]/float(N_p_i))
+
+            #n_min_txt = r"Numeric GS @ $\alpha = ${} with $\langle E \rangle / N_P =$ {:.2f}".format(num_min_cord[0], num_min_cord[1]/float(N_p_i))
+
+            ax.annotate(a_min_txt, xy=anal_min_cord,  xycoords='data',
+                    xytext=(50, 120), textcoords='offset points',
+                    arrowprops=dict(arrowstyle="->",
+                                    connectionstyle="arc3,rad=0.2"))
 
             '''
-            plt.errorbar(x, num, fmt = "x-",
-                    barsabove = True,
-                    yerr = np.array(stds[1], dtype = float),
-                    label = "numeric_energy")
+            ax.annotate(n_min_txt, xy=num_min_cord,  xycoords='data',
+                    xytext=(-60, 150), textcoords='offset points',
+                    arrowprops=dict(arrowstyle="->",
+                                    connectionstyle="arc3,rad=0.2"))        
             '''
-            errs = np.array(stds[0], dtype=float)
-            print(type(errs))
-            print(np.shape(errs))
-            print(anal," ",x)
+    plt.xlabel(r"$\alpha$")
+    plt.ylabel(r"$\langle E \rangle $")
 
-            plt.errorbar(x, anal, fmt = "^-",
-                    barsabove = True,
-                    yerr = errs,
-                    label = "analytic_energy") 
-            plt.title(title)
-            plt.xticks(x, rotation = 45, size = "medium")
-            plt.legend()
-
-        a_min_txt = r"Analytic GS @ $\alpha = ${} with $\langle E \rangle / N_P =$ {:.2f}".format(anal_min_cord[0], anal_min_cord[1]/float(N_p_i))
-
-        #n_min_txt = r"Numeric GS @ $\alpha = ${} with $\langle E \rangle / N_P =$ {:.2f}".format(num_min_cord[0], num_min_cord[1]/float(N_p_i))
-
-        ax.annotate(a_min_txt, xy=anal_min_cord,  xycoords='data',
-                xytext=(50, 120), textcoords='offset points',
-                arrowprops=dict(arrowstyle="->",
-                                connectionstyle="arc3,rad=0.2"))
-
-        '''
-        ax.annotate(n_min_txt, xy=num_min_cord,  xycoords='data',
-                xytext=(-60, 150), textcoords='offset points',
-                arrowprops=dict(arrowstyle="->",
-                                connectionstyle="arc3,rad=0.2"))        
-        '''
-        plt.xlabel(r"$\alpha$")
-        plt.ylabel(r"$\langle E \rangle $")
-
-        figname = sim_types[0]
-        figname += "_np_" + str(N_p_i) + "_nd_" + str(N_d_i) + "_c.pdf" 
-        plt.savefig("../report/figures/" + figname)
-        plt.clf()
+    figname = sim_types[0]
+    figname += "_np_" + str(N_p_i) + "_nd_" + str(N_d_i) + "_dt_" + str(dt) + ".pdf" 
+    plt.savefig("../report/figures/" + figname)
+    plt.clf()
