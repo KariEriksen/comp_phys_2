5789aca77295add610a920abe82d610ed08e3b63
Geir Ulvik <ulvik@usit.uio.no>
2018-05-04 19:05:02 +0200

    Modified drift force in wavefunc.h to take additional argument.

--- project_1/include/wavefunc.h
+++ project_1/include/wavefunc.h
@@ -17,7 +17,7 @@ class WaveFunc{
     
         virtual double evaluate(mat R) = 0; 
         virtual double E_l(mat R) = 0;
-        virtual mat drift_force(mat R) = 0;
+        virtual mat drift_force(mat R, int j) = 0;
         virtual double laplace(mat R) = 0;
         virtual double ratio(mat R, mat R_p, int k) = 0; 
        };
