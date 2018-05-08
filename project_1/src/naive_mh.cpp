495bbc857966e8bc8f19d5265fbd637270eabbfa
Geir Ulvik <ulvik@usit.uio.no>
2018-05-08 21:36:06 +0200

    checkout naive_mh from master to fix static energy.

--- project_1/src/naive_mh.cpp
+++ project_1/src/naive_mh.cpp
@@ -8,7 +8,6 @@ using namespace std;
 double NaiveMh::metropolis_hastings(WaveFunc *psi_t, double prev_E_l){
     mat R_p(size(R));
     R_p = R;
-	double beta = psi_t -> params[2];
 
     uniform_int_distribution<int> dis_r(0, N_p - 1);
     uniform_real_distribution<double> dis_step(-1, 1);
@@ -18,9 +17,6 @@ double NaiveMh::metropolis_hastings(WaveFunc *psi_t, double prev_E_l){
     for(int i = 0; i < N_d; i++){
         R_p(j, i) += dis_step(*gen) * step ;
     }
-	if(N_d == 3){
-		R_p(j,2) *= beta;
-	}
     double eps = dis_p(*gen);
     double P = psi_t -> ratio(R, R_p, j);
     /*
