cc7822b4c4f787f8ac29742a118273092bd337e0
KariEriksen <eriksen_kari@hotmail.com>
2018-06-04 14:55:22 +0200

    Written so far

--- project_2/src/main_b.cpp
+++ project_2/src/main_b.cpp
@@ -7,17 +7,28 @@ using namespace arma;
 
 int main(int argc, char *argv[]){
 
-    mat a, b, W;
     int N, M;
     int N_p, N_d, N_mc;
-    double gamma;
-    double step;
+    double gamma, omega, sigma, step;
+
+    //N = 3; N_p = 1; N_d = 2;
+    N = atoi(argv[1]) ; N_p = atoi(argv[1]);
+    N_d = atoi(argv[2]); mc_exp = atoi(argv[3]);
+
+    M = N_p*N_d;
+
+    N_mc = pow(2, mc_exp);
+
+    gamma = 1; omega = 1; sigma = 1; step = 0.1;
+
+    nqs n;
+    NaiveMh D;
+
+    D.step = step;
+
+    vector<double> result;
+    string filename = "filename";
+    result = D.solve(&n, filename);
 
 
-    for(int i = 0; i < N_mc; i++){
-
-        N = 3;
-        M = N_p*N_d;
-
-    }
 }
