6a11aaa0b564d566c7960732a1eabc665a1f1ac8
KariEriksen <eriksen_kari@hotmail.com>
2018-06-05 18:17:09 +0200

    added a b w as class params

--- project_2/include/nqs.h
+++ project_2/include/nqs.h
@@ -12,14 +12,14 @@ private:
     mat D_p;
 public:
     void set_params(int M, int N, int N_p, int N_d, double sigma, double omega, double gamma);
-    double ratio(mat R, mat R_p, int k, mat a, mat b, mat W);
-    double evaluate(mat R, mat a, mat b, mat W);
-    double E_l(mat R, mat a, mat b, mat W);
-    double laplace(mat R, mat a, mat b, mat W);
-    mat drift_force(mat R, mat a, mat b, mat W);
-    void initialize(mat a, mat b, mat W);
+    double ratio(mat R, mat R_p, int k);
+    double evaluate(mat R);
+    double E_l(mat R);
+    double laplace(mat R);
+    mat drift_force(mat R);
+    void initialize();
     void update_positions(mat R);
-    void update_weights(mat G, mat a, mat b, mat W);
+    void update_weights(mat G);
     //mat NumericalDoubleDerivative(mat R);
     //??
     nqs();
